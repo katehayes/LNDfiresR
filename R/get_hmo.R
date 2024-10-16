e7227f6d17444ae085e5cb3dc9c4e24f1f723150
katehayes <khayes2@sheffield.ac.uk>
2024-10-16 21:21:17 +0100

    tidying data extraction scripts

--- R/get_hmo.R
+++ R/get_hmo.R
@@ -1,10 +1,7 @@
 library(httr)
 library(jsonlite)
 
-# https://ons.gov.uk/datasets/RM193/editions/2021/versions/2?f=get-data
-# datasets/RM193/editions/2021/versions/2
-# hmo_data <- read.csv("/Users/katehayes/Library/CloudStorage/GoogleDrive-khayes2@sheffield.ac.uk/My Drive/THdata/RM193-2021-2.csv")
-
+# https://ons.gov.uk/datasets/RM193/editions/2021/versions/2
+temp1 <- tempfile()
 download.file("https://s3.eu-west-1.amazonaws.com/statistics.digitalresources.jisc.ac.uk/dkan/files/2021/ONS/number-of-households-that-are-in-HMO/by-accomodation-type/RM193-Number-Of-Households-In-Houses-In-Multiple-Occupation-(Hmo)-By-Accommodation-Type-2021-ltla-ONS.xlsx", temp1)
-
 raw_hmo <- read_xlsx(temp1)
